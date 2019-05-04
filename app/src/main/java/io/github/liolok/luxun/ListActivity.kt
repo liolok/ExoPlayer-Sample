16138359abb19a23a1feeb9e9c4143555af5df4e
liolok <liolokit@gmail.com>
2019-05-04 17:37:54 +0800

    feat: use grid of card to show items for list activity

--- app/src/main/java/io/github/liolok/luxun/ListActivity.kt
+++ app/src/main/java/io/github/liolok/luxun/ListActivity.kt
@@ -2,10 +2,27 @@ package io.github.liolok.luxun
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_list.*
+import kotlinx.serialization.ImplicitReflectionSerializer
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.parseList
+
+private const val GRID_SPAN_COUNT = 2  // grid layout column count
 
 class ListActivity : AppCompatActivity() {
+    private val items
+        @ImplicitReflectionSerializer get() = Json.parseList<Item>(  // read a list of item from `res/raw/items.json`
+            resources.openRawResource(R.raw.items).bufferedReader().use { it.readText() })  // convert to string
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        recyclerView.apply {
+            setHasFixedSize(true) // use this to improve performance if layout size of RecyclerView doesn't change
+            layoutManager = GridLayoutManager(this@ListActivity, GRID_SPAN_COUNT)  // use grid layout
+            adapter = RecyclerAdapter(items)  // pass item list to adapter
+        }
     }
 }
---
+++ app/src/main/java/io/github/liolok/luxun/RecyclerAdapter.kt
@@ -0,0 +1,33 @@
+package io.github.liolok.luxun
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item.view.*
+
+class RecyclerAdapter(private val items: List<Item>) : RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder>() {
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder =
+        ItemViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false))
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) = holder.bind(items[position])
+
+    // Return the size of item list (invoked by the layout manager)
+    override fun getItemCount() = items.size
+
+    // Provide a reference to the views for each item
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: Item) = with(itemView) {
+            title.text = item.title
+            thumb.setImageResource(
+                resources.getIdentifier("cover_${item.book}", "drawable", context.packageName)
+            )  // get drawable resource id by item's book name value, set thumb image
+            setOnClickListener {
+                // context.startActivity(Intent(context, DetailActivity::class.java).putExtra(Item.EXTRA_KEY, item))
+            }  // on item view clicked, start detail activity and add the item object as extra
+        }  // bind an item object to its view
+    }
+}
