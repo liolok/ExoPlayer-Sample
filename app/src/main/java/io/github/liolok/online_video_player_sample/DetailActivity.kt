9dc28997dfb702217923dc05cf08e5775abd0dbc
liolok <liolokit@gmail.com>
2019-05-31 00:26:16 +0800

    feat: set detail activity actionbar title

--- app/src/main/java/io/github/liolok/online_video_player_sample/DetailActivity.kt
+++ app/src/main/java/io/github/liolok/online_video_player_sample/DetailActivity.kt
@@ -8,11 +8,12 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val bundle = Bundle()  // get item object from MainActivity and put it into bundle
-        bundle.putParcelable(Item.EXTRA_KEY, intent.getParcelableExtra(Item.EXTRA_KEY))
+        val playerFragment = PlayerFragment()  // create a player fragment
 
-        val playerFragment = PlayerFragment()  // pass item object to player fragment through bundle
-        playerFragment.arguments = bundle
+        val item = intent.getParcelableExtra<Item>(Item.EXTRA_KEY)  // get item object from MainActivity
+        supportActionBar?.title = item.title  // set actionbar title with item's
+        val bundle = Bundle(); bundle.putParcelable(Item.EXTRA_KEY, item)  // pack item object up with a bundle
+        playerFragment.arguments = bundle  // pass item object to player fragment through bundle
 
         supportFragmentManager.beginTransaction().add(R.id.detailContainer, playerFragment).commit()  // show fragment
     }
